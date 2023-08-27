a9520c34ac3f2e99a94a841961084bb18cd1129a
marcoleka <marcoleka19@gmail.com>
2023-08-28 00:24:31 +0200

    up

--- index.js
+++ index.js
@@ -22,29 +22,3 @@ import { ActionManager } from '#src/actions/ActionManager';
     await db.end();
   }
 })();
-
-
-
-/*(async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: false, executablePath: "/home/marco/.cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome" });
-  const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto('https://webscraper.io/test-sites/e-commerce/static');
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Wait and click on first result
-  const searchResultSelector = '::-p-xpath(/html/body/div[1]/div[2]/div/div/div/h1)';
-  const textSelector = await page.waitForSelector(searchResultSelector);
-
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-
-  await browser.close();
-
-})();*/
